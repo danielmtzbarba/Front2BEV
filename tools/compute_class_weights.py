68f7004f76b32851d0cbd256bf993019fda6f8cc
danielmtz <danielmtzbarba@gmail.com>
2023-10-03 23:21:36 -0600

    compute class_weights.

--- tools/compute_class_weights.py
+++ tools/compute_class_weights.py
@@ -1,38 +1,29 @@
 from pathlib import Path
 from tqdm import tqdm
 import numpy as np
-import cv2
+import pandas as pd
 
-from dan.utils.data import get_filenames_list
-from utils import get_test_dirs
+
 import utils.bev as bev
-
-DATASET_PATH = Path("/media/aisyslab/dan/Datasets/Dan-2023-Front2BEV/")
 
 msk = bev.mask64.copy()
 print("\nMASK64:", np.unique(msk, return_counts=True)[1], '\n')
 
-def get_class_weights(dataset_path, n_classes):
+def get_class_weights(img_dataset, n_classes):
     pixel_count = {key:value for (key,value) in enumerate([0 for i in range(n_classes + 1)])}
     pixel_count_fov = {key:value for (key,value) in enumerate([0 for i in range(n_classes)])}
-
-    test_paths = get_test_dirs(dataset_path)
 
     total_pixels = 0
     total_fov_pixels = 0
 
-    for test_path in tqdm(test_paths):
-        bev_raw_path = test_path / "bev" / f"{n_classes}k"
-        bev_imgs = get_filenames_list(bev_raw_path, ".png")
+    for i in tqdm(range(len(img_dataset))):
+        bev_img = img_dataset[i]
 
-        for bev_img_name in bev_imgs:
-            bev_img = cv2.imread(str(bev_raw_path / bev_img_name), cv2.IMREAD_GRAYSCALE)
+        pixel_count, n_pixels = bev.count_pixels(bev_img, pixel_count, n_classes, False)
+        total_pixels += n_pixels
 
-            pixel_count, n_pixels = bev.count_pixels(bev_img, pixel_count, n_classes, False)
-            total_pixels += n_pixels
-
-            pixel_count_fov, n_fov_pixels = bev.count_pixels(bev_img, pixel_count_fov, n_classes, True)
-            total_fov_pixels += n_fov_pixels
+        pixel_count_fov, n_fov_pixels = bev.count_pixels(bev_img, pixel_count_fov, n_classes, True)
+        total_fov_pixels += n_fov_pixels
             
     weights = {key:(total_pixels/value) for (key,value) in pixel_count.items()}
     weights_fov = {key:(total_fov_pixels/value) for (key,value) in pixel_count_fov.items()}
@@ -45,10 +36,46 @@ def get_class_weights(dataset_path, n_classes):
     print('FOV_Pixel_count:', pixel_count_fov)
     print('Class weights (FOV):', weights_fov, '\n')
 
+    return [total_pixels, str(pixel_count), str(weights), total_fov_pixels,
+             str(pixel_count_fov), str(weights_fov)]
+
+
+from dan.utils.torch.datasets import ImageDataset
+
+csv_path = Path('__datasets/Dan-2023-Front2bev/')
+filename = 'front2bev_'
+root_path = '/media/dan/dan/Datasets/'
+
 def main():
 
+    for config in ['layers_all', 'layers_none']:
+        weight_data = []
+
+        for n in [2, 3]:
+            path = csv_path / config / f"{n}k" / f"{filename}{n}k.csv"
+            df = pd.read_csv(path, header=None, usecols=[3]).drop(0)
+            df = df.apply(lambda path: (root_path + path))
+            bev_dataset = ImageDataset(df.values)
+            wdata = get_class_weights(bev_dataset, n)
+            weight_data.append([config, n, *wdata])
+            weights_df = pd.DataFrame(weight_data, columns=['config', 'n_classes', 'total_pix', 'pix_count',
+                                        'weights', 'total_fov_pix','fov_pix_count','fov_weights'])
+            weights_df.to_csv(str(csv_path / config / f'{n}k' / f'weights_{n}k.csv'), index=False)
+
+    config = 'traffic'
+    for n in [2, 3, 4, 5, 6]:
+        weight_data = []
+        path = csv_path / config / f"{n}k" / f"{filename}{n}k.csv"
+        df = pd.read_csv(path, header=None, usecols=[3]).drop(0)
+        df = df.apply(lambda path: (root_path + path))
+        bev_dataset = ImageDataset(df.values)
+        wdata = get_class_weights(bev_dataset, n)
+        weight_data.append([config, n, *wdata])
+
+        weights_df = pd.DataFrame(weight_data, columns=['config', 'n_classes', 'total_pix', 'pix_count',
+                                            'weights', 'total_fov_pix','fov_pix_count','fov_weights'])
+        weights_df.to_csv(str(csv_path / config / f'{n}k' / f'weights_{n}k.csv'), index=False)
    
-    
 
 if __name__ == '__main__':
   main()
