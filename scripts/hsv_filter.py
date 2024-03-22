62741ca2392b7d24d9caf67b271808acac901dc6
Daniel Martinez <danielmtz.dev@gmail.com>
2024-03-22 15:45:38 -0600

    hsv color detection

---
+++ scripts/hsv_filter.py
@@ -0,0 +1,85 @@
+import cv2
+import os
+import sys
+import numpy as np
+
+def nothing(x):
+    pass
+
+def hsv_tool(image):
+# Create a window
+    cv2.namedWindow('image')
+
+# create trackbars for color change
+    cv2.createTrackbar('HMin','image',0,179,nothing) # Hue is from 0-179 for Opencv
+    cv2.createTrackbar('SMin','image',0,255,nothing)
+    cv2.createTrackbar('VMin','image',0,255,nothing)
+    cv2.createTrackbar('HMax','image',0,179,nothing)
+    cv2.createTrackbar('SMax','image',0,255,nothing)
+    cv2.createTrackbar('VMax','image',0,255,nothing)
+
+# Set default value for MAX HSV trackbars.
+    cv2.setTrackbarPos('HMax', 'image', 179)
+    cv2.setTrackbarPos('SMax', 'image', 255)
+    cv2.setTrackbarPos('VMax', 'image', 255)
+
+# Initialize to check if HSV min/max value changes
+    hMin = sMin = vMin = hMax = sMax = vMax = 0
+    phMin = psMin = pvMin = phMax = psMax = pvMax = 0
+
+    output = image
+    wait_time = 33
+
+    while(1):
+
+        # get current positions of all trackbars
+        hMin = cv2.getTrackbarPos('HMin','image')
+        sMin = cv2.getTrackbarPos('SMin','image')
+        vMin = cv2.getTrackbarPos('VMin','image')
+
+        hMax = cv2.getTrackbarPos('HMax','image')
+        sMax = cv2.getTrackbarPos('SMax','image')
+        vMax = cv2.getTrackbarPos('VMax','image')
+
+        # Set minimum and max HSV values to display
+        lower = np.array([hMin, sMin, vMin])
+        upper = np.array([hMax, sMax, vMax])
+
+        # Create HSV Image and threshold into a range.
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, lower, upper)
+        output = cv2.bitwise_and(image,image, mask= mask)
+
+        # Print if there is a change in HSV value
+        if( (phMin != hMin) | (psMin != sMin) | (pvMin != vMin) | (phMax != hMax) | (psMax != sMax) | (pvMax != vMax) ):
+            print("(hMin = %d , sMin = %d, vMin = %d), (hMax = %d , sMax = %d, vMax = %d)" % (hMin , sMin , vMin, hMax, sMax , vMax))
+            phMin = hMin
+            psMin = sMin
+            pvMin = vMin
+            phMax = hMax
+            psMax = sMax
+            pvMax = vMax
+
+        # Display output image
+        cv2.imshow('image',output)
+
+        # Wait longer to prevent freeze for videos.
+        if cv2.waitKey(wait_time) & 0xFF == ord('q'):
+            break
+
+    cv2.destroyAllWindows()
+
+IMG_DIR = '/media/dan/data/datasets/Dan-2024-F2B-Autominy'
+TRACKS = ['Track01']
+SCENES = ['scene-1']
+
+if __name__ == '__main__':
+    for track in TRACKS:
+        for scene in SCENES:
+            scene_dir = os.path.join(IMG_DIR, track, scene, "rawbev")
+            imgs = os.listdir(scene_dir)
+            for img_name in imgs:
+                img = cv2.imread(os.path.join(scene_dir, img_name),
+                                 cv2.IMREAD_COLOR)
+                hsv_tool(img)
+                break
