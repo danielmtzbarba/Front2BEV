142f1541411de068ad460db08a7cea032a803815
Daniel Martinez <danielmtz.dev@gmail.com>
2024-08-16 13:51:10 -0600

    {map: [scenes]} dicts to generate dataset splits

--- scripts/create_dataset_csv.py
+++ scripts/create_dataset_csv.py
@@ -1,141 +1,120 @@
 import os
 import pandas as pd
 
+
 class Dataset(object):
-    def __init__(self, maps=[], scenes=[]):
+    def __init__(self, maps={}):
         self._maps = maps
-        self._scenes = scenes
-        self._samples = [] 
+        self._samples = []
         self._columns = ["map", "scene", "map_config", "sample"]
         self._base_df = pd.DataFrame([], columns=self._columns)
 
     def get_dataset(self, datadir, configs=["layers_none", "layers_all", "traffic"]):
-        
-        for map in self._maps:
+        for map in self._maps.keys():
             mapdir = os.path.join(datadir, map)
-            for scene in self._scenes:
+            for scene in self._maps[map]:
                 for config in configs:
                     sampledir = os.path.join(mapdir, scene, config, "rgb")
                     for s in os.listdir(sampledir):
                         sample = [map, scene, config, s]
-                        self._samples.append(sample) 
+                        self._samples.append(sample)
 
-        self._df = pd.concat([self._base_df, pd.DataFrame(self._samples, columns=self._columns)], ignore_index=True, axis=0)
+        self._df = pd.concat(
+            [self._base_df, pd.DataFrame(self._samples, columns=self._columns)],
+            ignore_index=True,
+            axis=0,
+        )
 
-    def split(self, scenes=[]):
-
+    def split(self, scenes={}):
         df = self._base_df.copy(deep=True)
-        
-        for scene in scenes:
-            df = pd.concat([df, self._df[self._df["scene"] == scene]], ignore_index=True)
+
+        for map in scenes.keys():
+            for scene in scenes[map]:
+                map_samples = self._df[self._df["map"] == map]
+                scene_samples = map_samples[map_samples["scene"] == scene]
+
+                df = pd.concat([df, scene_samples], ignore_index=True)
 
         return df
 
-    def to_csv(self, data, output_path,  augmented=False):
-
+    def to_csv(self, data, output_path, augmented=False):
         samples = []
         filtered = self._base_df.copy(deep=True)
-        
+
         if augmented:
-            configs = ['layers_none', 'layers_all', 'traffic']
+            configs = ["layers_none", "layers_all", "traffic"]
         else:
-            configs = ['traffic']
+            configs = ["traffic"]
 
         for config in configs:
-            filtered = pd.concat([filtered, data[data["map_config"] == config]], ignore_index=True)
+            filtered = pd.concat(
+                [filtered, data[data["map_config"] == config]], ignore_index=True
+            )
 
         for row in filtered.itertuples(index=False):
-            path_rgb = f'{row.map}/{row.scene}/{row.map_config}/rgb/{row.sample}'
-            path_bev = f'{row.map}/{row.scene}/{row.map_config}/bev/$k/{row.sample}'
+            path_rgb = f"{row.map}/{row.scene}/{row.map_config}/rgb/{row.sample}"
+            path_bev = f"{row.map}/{row.scene}/{row.map_config}/bev/$k/{row.sample}"
             path_bev = path_bev.replace(".jpg", ".png")
-            path_rgbd = f'{row.map}/{row.scene}/{row.map_config}/rgbd/{row.sample}'
+            path_rgbd = f"{row.map}/{row.scene}/{row.map_config}/rgbd/{row.sample}"
             samples.append([path_rgb, path_bev, path_rgbd])
-        
-        print(configs, len(samples))
+
+        print(configs, len(samples), output_path)
         pd.DataFrame(samples).to_csv(output_path, header=False, index=False)
 
-    @property 
+    @property
     def df(self):
         return self._df
 
     def __len__(self):
         return len(self._df)
 
-# -----------------------------------------------------------------------------------
-def save_dataset(dataset, split_scenes,  dataset_path,
-                 phase='train', augmented=False):
 
+# -----------------------------------------------------------------------------------
+def save_dataset(dataset, split_scenes, dataset_path, phase="train", augmented=False):
     split = dataset.split(split_scenes)
-    dataset.to_csv(split, os.path.join(dataset_path, f'front2bev-{phase}.csv'),  augmented=augmented)
+    dataset.to_csv(
+        split, os.path.join(dataset_path, f"front2bev-{phase}.csv"), augmented=augmented
+    )
+
 
 # -----------------------------------------------------------------------------------
-DATADIR = '/media/aisyslab/CHINA/Front2BEV-RGBD'
+DATADIR = "/run/media/dan/dan/datasets/Front2BEV-RGBD"
 
-#DATADIR = '/home/aircv1/Data/Luis/aisyslab/Daniel/Datasets/Dan-2024-Front2BEV'
-traffic_dataset_path = 'datasets/Front2BEV-RGBD/'
-augmented_dataset_path= 'datasets/Dan-2024-Front2BEV-Augmented/'
+# DATADIR = '/home/aircv1/Data/Luis/aisyslab/Daniel/Datasets/Dan-2024-Front2BEV'
+traffic_dataset_path = "datasets/f2b-rgbd/"
+augmented_dataset_path = "datasets/Dan-2024-Front2BEV-Augmented/"
+
 
 # -----------------------------------------------------------------------------------
-def main():
-# ONLY TRAFFIC, 1 MAP - 9 DIFFERENT SCENES,
-# SEQ10 VAL, SEQ11 TEST
-    MAPS = ['Town01']
-    SCENES = [f'scene_{i}' for i in range(1, 12)]
+def create_rgbd_dataset():
+    MAPS = {}
+    MAPS["Town01"] = [f"scene_{i}" for i in range(1, 16)]
+    MAPS["Town02"] = [f"scene_{i}" for i in range(1, 11)]
+    MAPS["Town03"] = [f"scene_{i}" for i in range(1, 21)]
+    MAPS["Town04"] = [f"scene_{i}" for i in range(1, 21)]
 
-    TRAIN = [f'scene_{i}' for i in range(1, 10)] 
-    VAL = [f'scene_{i}' for i in range(10, 11)] 
-    TEST = [f'scene_{i}' for i in range(11, 12)] 
-    dataset = Dataset(maps=MAPS, scenes=SCENES)
-    dataset.get_dataset(DATADIR) 
-    save_dataset(dataset,  TRAIN, traffic_dataset_path, phase="train", augmented=False)
-    save_dataset(dataset,  VAL, traffic_dataset_path, phase="val", augmented=False)
-    save_dataset(dataset,  TEST, traffic_dataset_path, phase="test", augmented=False)
+    TRAIN = {}
+    TRAIN["Town01"] = [f"scene_{i}" for i in range(1, 14)]
+    TRAIN["Town02"] = [f"scene_{i}" for i in range(1, 10)]
+    TRAIN["Town03"] = [f"scene_{i}" for i in range(1, 20)]
+    TRAIN["Town04"] = [f"scene_{i}" for i in range(1, 20)]
 
+    VAL = {}
+    VAL["Town01"] = ["scene_14", "scene_15"]
+    VAL["Town02"] = ["scene_10"]
+    VAL["Town03"] = ["scene_20"]
+    VAL["Town04"] = ["scene_20"]
 
-# AUGMENTED, 1  MAP -  9  DIFFERENT SCENES, T
-# SEQ10 VAL, SEQ11 TEST
-#    MAPS = ['Town01']
-#    SCENES = [f'scene_{i}' for i in range(1, 12)]
-
-#    TRAIN = [f'scene_{i}' for i in range(1, 10)] 
-#    VAL = [f'scene_{i}' for i in range(10, 11)] 
-#    TEST = [f'scene_{i}' for i in range(11, 12)] 
-#    dataset = Dataset(maps=MAPS, scenes=SCENES)
-#    dataset.get_dataset(DATADIR)
-#    save_dataset(dataset,  (TRAIN, VAL, TEST), augmented_dataset_path, augmented=True)
-
-# AUGMENTED, 3 MAPS -  3 DIFFERENT SCENES,
-# SEQ10 VAL, SEQ11 TEST
-#    MAPS = ['Town01', 'Town02', 'Town03']
-#    SCENES = [f'scene_{i}' for i in range(1, 4)]
-
-#    AUG_TRAIN = [f'scene_{i}' for i in range(1, 2)] 
-#    dataset = Dataset(maps=MAPS, scenes=SCENES)
-#    dataset.get_dataset(DATADIR)
-#    save_dataset(dataset,  (AUG_TRAIN, VAL, TEST), augmented_dataset_path, augmented=True)
-
-   # TEST  
-    MAPS = [ 'Town02']
-    SCENES = [f'scene_{i}' for i in range(1, 4)]
-
-    TEST = [f'scene_{i}' for i in range(1, 4)] 
-    dataset = Dataset(maps=MAPS, scenes=SCENES)
+    dataset = Dataset(maps=MAPS)
     dataset.get_dataset(DATADIR)
-    save_dataset(dataset, TEST, 'datasets/Front2BEV-RGBD-test/',
-                 phase='test', augmented=False)
+    print("F2B-RGBD:", len(dataset))
 
-if __name__ == '__main__':
-   # TEST 
+    save_dataset(dataset, TRAIN, traffic_dataset_path, phase="train", augmented=False)
+    save_dataset(dataset, VAL, traffic_dataset_path, phase="val", augmented=False)
 
-#    DATADIR = "/media/dan/data/datasets/Dan-2024-F2B-Autominy/"
-#    MAPS = [ 'Track01']
-#    SCENES = [f'scene-{i}' for i in range(1, 3)]
 
-#    TRAIN = [f'scene-{i}' for i in range(1, 10)] 
-#    dataset = Dataset(maps=MAPS, scenes=SCENES)
-#    dataset.get_dataset(DATADIR, ['traffic'])
-#    save_dataset(dataset, TRAIN, 'datasets/Dan-2024-F2B-Autominy/',
-#                 phase='train', augmented=False)
+# save_dataset(dataset, TEST, traffic_dataset_path, phase="test", augmented=False)
 
-    main()
-     
+
+if __name__ == "__main__":
+    create_rgbd_dataset()
--- scripts/generate_classic_augmentation_imgs.py
+++ scripts/generate_classic_augmentation_imgs.py
@@ -7,23 +7,29 @@ import matplotlib.pyplot as plt
 
 from src.data.front2bev.utils import process_path
 
+
 def load_image(image_path, rgb=True):
     """Load an image from the specified path."""
     if rgb:
         image = cv2.imread(image_path)
     else:
-        image = cv2.imread(image_path,  cv2.IMREAD_GRAYSCALE)
+        image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
     return image
+
 
 def flip_image(image):
     """Flip the input image horizontally."""
-    flipped_image = cv2.flip(image, 1)  # 1 for horizontal flip, 0 for vertical flip, -1 for both flips
+    flipped_image = cv2.flip(
+        image, 1
+    )  # 1 for horizontal flip, 0 for vertical flip, -1 for both flips
     return flipped_image
+
 
 def blur_image(image, kernel_size=(39, 39)):
     """Apply Gaussian blur to the input image."""
     blurred_image = cv2.GaussianBlur(image, kernel_size, 0)
     return blurred_image
+
 
 def plot_images(image1, image2):
     """Plot three images in a grid of 1 row and 2 columns."""
@@ -31,25 +37,26 @@ def plot_images(image1, image2):
 
     # Plot first image
     axes[0].imshow(image1)
-    axes[0].set_title('original')
+    axes[0].set_title("original")
 
     # Plot second image
     axes[1].imshow(image2)
-    axes[1].set_title('augmented')
+    axes[1].set_title("augmented")
 
     # Hide axis for all plots
     for ax in axes:
-        ax.axis('off')
+        ax.axis("off")
 
     plt.tight_layout()
     plt.show()
 
+
 def save_rgb(image, row, method):
     """Save the image to the specified path."""
-    dirs = row.split('/')
-    dirs[len(dirs)-3] = method 
-    outdir, name = dirs[:-1], dirs[len(dirs)-1]
-    path = '/'.join(outdir)
+    dirs = row.split("/")
+    dirs[len(dirs) - 3] = method
+    outdir, name = dirs[:-1], dirs[len(dirs) - 1]
+    path = "/".join(outdir)
     try:
         os.makedirs(path)
     except:
@@ -58,12 +65,13 @@ def save_rgb(image, row, method):
     cv2.imwrite(imgpath, image)
     return imgpath
 
+
 def save_bev(image, row, method):
     """Save the image to the specified path."""
-    dirs = row.split('/') 
-    dirs[len(dirs)-4] = method 
-    outdir, name = dirs[:-1], dirs[len(dirs)-1]
-    path = '/'.join(outdir)
+    dirs = row.split("/")
+    dirs[len(dirs) - 4] = method
+    outdir, name = dirs[:-1], dirs[len(dirs) - 1]
+    path = "/".join(outdir)
     try:
         os.makedirs(path)
     except:
@@ -77,37 +85,39 @@ def augment(dataset, method):
     df = dataset.copy()
     for i, row in dataset.iterrows():
         rgb_img = load_image(row[0])
-        if method == 'blur':
-            aug_img = blur_image(rgb_img)  
-            rgbpath =  save_rgb(aug_img, row[0],method)
-        if 'flip' in method: 
-            aug_img = flip_image(rgb_img)  
-            rgbpath = save_rgb(aug_img, row[0],method)
-            bev_img = Image.open(row[1]) 
-            bev_aug = ImageOps.mirror(bev_img) 
-            bevpath = save_bev(bev_aug, row[1],method)
-            df[1][i] = bevpath 
+        if method == "blur":
+            aug_img = blur_image(rgb_img)
+            rgbpath = save_rgb(aug_img, row[0], method)
+        if "flip" in method:
+            aug_img = flip_image(rgb_img)
+            rgbpath = save_rgb(aug_img, row[0], method)
+            bev_img = Image.open(row[1])
+            bev_aug = ImageOps.mirror(bev_img)
+            bevpath = save_bev(bev_aug, row[1], method)
+            df[1][i] = bevpath
         df[0][i] = rgbpath
-    return df 
- 
+    return df
 
-csv_dataset_path = 'datasets/Dan-2024-Front2BEV/front2bev-train.csv'
-#dataset_path = '/media/dan/dan/datasets/Dan-2024-Front2BEV/'
-dataset_path = '/home/aircv1/Data/Luis/aisyslab/Daniel/Datasets/Dan-2024-Front2BEV/'
+
+csv_dataset_path = "datasets/Dan-2024-Front2BEV/front2bev-train.csv"
+dataset_path = "/run/dan/dan/datasets/Dan-2024-Front2BEV/"
+# dataset_path = '/home/aircv1/Data/Luis/aisyslab/Daniel/Datasets/Dan-2024-Front2BEV/'
+
 
 def main():
     # Change dataset relative paths to absolute paths
     df = pd.read_csv(csv_dataset_path, header=None)
     df = process_path(df, dataset_path, 5, "traffic")
 
-    aug_dataset =  augment(df, 'flip')
+    aug_dataset = augment(df, "flip")
     df_aug = pd.concat([df, aug_dataset], ignore_index=True)
 
-    aug_dataset =  augment(df, 'blur')
+    aug_dataset = augment(df, "blur")
     dfout = pd.concat([df_aug, aug_dataset], ignore_index=True)
 
-#    dfout.to_csv('test.csv', header=False, index=False)
-    return 
+    #    dfout.to_csv('test.csv', header=False, index=False)
+    return
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
