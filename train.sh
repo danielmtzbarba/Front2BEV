f2ced911c57c840a0637326de1515761a165b428
Daniel Martinez <danielmtz.dev@gmail.com>
2024-08-19 17:25:19 -0600

    train script fix

--- train.sh
+++ train.sh
@@ -3,9 +3,9 @@
 #python train.py --experiment=f2b-pon_depth-rgbd --dataset=f2b-rgbd --model=pon_depth --optimizer=adam --pc=msi
 
 # VED
-#python train.py --experiment=f2b-ved-rgb --dataset=f2b-rgbd --map_config=traffic --model=ved --optimizer=adam --pc=home
-#python train.py --experiment=f2b-rgved-rgbd --dataset=f2b-rgbd --map_config=traffic --model=rgved --optimizer=adam --pc=home
+#python train.py --experiment=f2b-ved-rgb --dataset=f2b-rgbd --model=ved --optimizer=adam --pc=home
+#python train.py --experiment=f2b-rgved-rgbd --dataset=f2b-rgbd --model=rgved --optimizer=adam --pc=home
 
 #
-python train.py --experiment=f2b-vedfusion-rgbd --dataset=f2b-rgbd --map_config=traffic --model=vedfusion --optimizer=adam --pc=home
-#python train.py --experiment=f2b-ponfusion-rgbd --dataset=f2b-rgbd --map_config=traffic --model=ponfusion --optimizer=adam --pc=home
+python train.py --experiment=f2b-vedfusion-rgbd --dataset=f2b-rgbd --model=ved_fusion --optimizer=adam --pc=msi
+#python train.py --experiment=f2b-ponfusion-rgbd --dataset=f2b-rgbd --model=ponfusion --optimizer=adam --pc=home
