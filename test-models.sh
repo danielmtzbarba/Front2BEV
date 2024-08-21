7cdb7d739ce0188dd62befa4c713fba66b76bb5c
DanielMtzBarba <danielmtzbarba@gmail.com>
2024-08-21 08:32:26 -0600

    clean project

--- test-models.sh
+++
@@ -1,4 +0,0 @@
-python test.py --experiment=f2b --model=pon --dataset=front2bev-rgbd --map_config=traffic --optimizer=adam --weight_mode=sqrt_inverse --pc=aisyslab
-python test.py --experiment=f2b --model=ved --dataset=front2bev-rgbd --map_config=traffic --optimizer=adam --weight_mode=sqrt_inverse --pc=aisyslab
-python test.py --experiment=f2b-rgbd --model=rgved --dataset=front2bev-rgbd --map_config=traffic --optimizer=adam --weight_mode=sqrt_inverse --pc=aisyslab
-python test.py --experiment=f2b-rgbd --model=rgved_2h --dataset=front2bev-rgbd --map_config=traffic --optimizer=adam --weight_mode=sqrt_inverse --pc=aisyslab
